7d601db6ae0dc8a73aa956fd392f154e32e14684
ko1keith <ko.keith@hotmail.com>
2020-10-19 19:22:07 -0400

    finish get_user resource

--- server/resources/user_get.py
+++ server/resources/user_get.py
@@ -1,6 +1,5 @@
-from flask_restful import Resource, reqparse
-from flask_jwt_extended import (create_access_token, jwt_required, get_jwt_identity)
-import datetime
+from flask_restful import Resource
+from flask_jwt_extended import (jwt_required, get_jwt_identity)
 from server.models.user_model import UserModel
 import json
 
