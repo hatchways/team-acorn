092b714c1a0bfb38b91f7a262f721b6dcf3e2fd9
Keith <ko.keith@hotmail.com>
2020-10-21 17:57:55 -0400

    save run.py file

--- server/run.py
+++ server/run.py
@@ -21,11 +21,9 @@ from server.resources.reset_user_review_count import ResetUserReviewCount
 from server.extensions import db, api, jwt, create_app
 
 
-
 def create_api(app):
     print("inside create_api")
     # register api
-    
 
     api.add_resource(UserRegistration, '/registration')
     api.add_resource(UserLogin, '/login')
@@ -45,8 +43,6 @@ def create_api(app):
     return api
 
 
-
-
 app = create_app()
 api = create_api(app)
 
@@ -56,27 +52,9 @@ def create_tables():
     db.create_all()
 
 
-
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     # @ decorator is called every time client tries to access secured endpoint
     # function will return True or False depending if the passed token is blacklisted
     jti = decrypted_token['jti']
     return revoked_token_model.RevokedTokenModel.is_jti_blacklisted(jti)
-
-<<<<<<< HEAD
-
-from server.resource.user_logout_access import UserLogoutAccess
-from server.resource.user_login import UserLogin
-from server.resource.user_registration import UserRegistration
-from server.models import revoked_token_model
-from server.resource.user_experience import UserExperience
-from server.resource.user_get import UserGet
-
-api.add_resource(UserRegistration, '/registration')
-api.add_resource(UserLogin, '/login')
-api.add_resource(UserLogoutAccess, '/logout/access')
-api.add_resource(UserExperience, '/experience')
-api.add_resource(UserGet, '/user')
-=======
->>>>>>> 71d3ba85d7ea8b8b7f714981f17a1e1afa05587c
